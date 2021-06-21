fa07e7fbf39b263a0ddf5d6da39afb2495187d08
juntae <rlawnsxo131@gmail.com>
2021-06-21 22:36:00 +0900

    initial commit

---
+++ src/graphql/user.ts
@@ -0,0 +1,37 @@
+import { gql, IResolvers } from 'apollo-server';
+
+export const typeDef = gql`
+  type User @key(fields: "id") {
+    id: ID!
+    name: String!
+  }
+  extend type Query {
+    user(id: ID!): User
+    users: [User]
+  }
+`;
+
+export const resolvers: IResolvers = {
+  User: {},
+  Query: {
+    user: (parent, args, context, info) => {
+      const { id } = args;
+      return users.find((v) => v.id === id);
+    },
+    users: () => {
+      return users;
+    },
+  },
+  Mutation: {},
+};
+
+const users = [
+  {
+    id: '1',
+    name: 'juntae',
+  },
+  {
+    id: '2',
+    name: 'john',
+  },
+];
